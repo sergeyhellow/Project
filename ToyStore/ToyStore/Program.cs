010876da2a80cdab11577ffa0cb8c23e93c1e512
sergey <goblinsan@mail.ru>
2023-02-14 13:03:53 +0300

    add new forms

--- ToyStore/ToyStore/Program.cs
+++ ToyStore/ToyStore/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ToyStore.Views;
 
 namespace ToyStore
 {
@@ -16,7 +17,7 @@ namespace ToyStore
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new ListsOfToysForm());
         }
     }
 }
